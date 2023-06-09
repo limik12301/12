78815f17f336bb5fd6c45d8239c99a7151201a82
limik12301 <136087600+limik12301@users.noreply.github.com>
2023-06-09 15:41:13 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp6
+{
+    internal class FileName
+    {
+        static int index(int[] myArr, int a)
+        {
+            
+            for (int i = 0; i < myArr.Length; i++)
+            {
+                
+                if (myArr[i] == a)
+                    return i;        
+            }
+            return -1;
+        }
+        static void Main(string[] args)
+        {
+
+            int[] myArr = { 1, 76, 3, 4, 2 };
+            Console.WriteLine(index(myArr, 8));
+
+
+
+        }
+    }
+}
